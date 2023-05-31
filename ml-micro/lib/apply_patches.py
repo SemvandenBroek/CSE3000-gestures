be7125685ff823a0e4d00745ce35769d1f37b0c3
Sem <s-broek@outlook.com>
2023-05-31 21:20:06 +0200

    Update apply_patches.py for Windows

--- ml-micro/lib/apply_patches.py
+++ ml-micro/lib/apply_patches.py
@@ -1,11 +1,15 @@
 import os
+import platform
 
 env = DefaultEnvironment()
+
+err_redir = "/dev/null" if platform.system() == "Linux" else "nul"
 
 # Because of a stupid bug in the flatbuffers library and a version mismatch with TFLite-Micro when flatbuffers is updated, we have to apply this patch
 def apply_git_patches():
     os.chdir("../lib/flatbuffers/")
-    applied = os.system("git apply ../flatbuffers.patch 2>/dev/null")
+
+    applied = os.system(f"git apply ../flatbuffers.patch 2>{err_redir}")
     if applied == 0:
         print("Applied flatbuffers patch")
     else:
